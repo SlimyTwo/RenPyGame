30091f983c1029d4aeaaabc2ee30d88b65713549
Alexandre Torres <wildrecon11@gmail.com>
2025-04-05 16:56:03 -0700

    Deleted BackgroundRenderer.py as it was redundant with DrawMainMenuBackground.py

--- utility/BackgroundRenderer.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import utility.GlobalVariables as gv  # gv = global variable alias
-
-def DrawMainMenuBackground():
-    screen_width, screen_height = gv.screen.get_size()
-    scaled_background = pygame.transform.scale(gv.background, (screen_width, screen_height))
-    gv.screen.blit(scaled_background, (0, 0))
